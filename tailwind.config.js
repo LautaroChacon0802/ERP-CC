6eb5a0d73e647bc5e3d7b2d18f013c325c035e66
Lautaro Zanazzi <lautarochacon0802@gmail.com>
2026-02-07 14:49:02 -0300

    Downgrade tailwind v3

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // Mantenemos extensión vacía para no romper el diseño existente
+    },
+  },
+  plugins: [],
+}
